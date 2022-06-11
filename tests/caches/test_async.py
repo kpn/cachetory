540c3d764702b26ce3868232257e74b1179c8244
Pavel Perestoronin <eigenein@gmail.com>
2022-06-12 01:44:19 +0200

    NEW: add a couple of integration tests for Redis

--- tests/caches/test_async.py
+++ tests/caches/test_async.py
@@ -1,18 +1,27 @@
-from pytest import fixture, mark
+from pytest import mark
 
 from cachetory import backends, serializers
 from cachetory.caches.async_ import Cache
 
-
-@fixture
-async def cache() -> Cache[int]:
-    return Cache(
-        serializer=serializers.from_url("pickle+zstd://"),
-        backend=(await backends.async_.from_url("memory://")),
-    )
+_test_redis = mark.skipif("not config.getoption('test_redis')")
 
 
 @mark.asyncio
-async def test_get_set(cache: Cache[int]):
+async def test_get_set_memory():
+    cache = Cache[int](
+        serializer=serializers.from_url("pickle+zstd://"),
+        backend=(await backends.async_.from_url("memory://")),
+    )
+    await cache.set("foo", 42)
+    assert await cache.get("foo") == 42
+
+
+@_test_redis
+@mark.asyncio
+async def test_get_set_in_redis():
+    cache = Cache[int](
+        serializer=serializers.from_url("pickle+zstd://pickle-protocol=5&compression-level=3"),
+        backend=(await backends.async_.from_url("redis://localhost:6379")),
+    )
     await cache.set("foo", 42)
     assert await cache.get("foo") == 42
--- tests/caches/test_sync.py
+++ tests/caches/test_sync.py
@@ -1,17 +1,25 @@
-from pytest import fixture
+from pytest import mark
 
 from cachetory import backends, serializers
 from cachetory.caches.sync import Cache
 
+_test_redis = mark.skipif("not config.getoption('test_redis')")
 
-@fixture
-def cache() -> Cache[int]:
-    return Cache(
+
+def test_get_set_in_memory():
+    cache = Cache[int](
         serializer=serializers.from_url("pickle+zstd://pickle-protocol=5&compression-level=3"),
         backend=backends.sync.from_url("memory://"),
     )
+    cache.set("foo", 42)
+    assert cache.get("foo") == 42
 
 
-def test_get_set(cache: Cache[int]):
+@_test_redis
+def test_get_set_in_redis():
+    cache = Cache[int](
+        serializer=serializers.from_url("pickle+zstd://pickle-protocol=5&compression-level=3"),
+        backend=backends.sync.from_url("redis://localhost:6379"),
+    )
     cache.set("foo", 42)
     assert cache.get("foo") == 42
