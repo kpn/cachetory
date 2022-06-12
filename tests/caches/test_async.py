50c24df3ae6d04ce58ef8a8114de1de4ae09a3f7
Pavel Perestoronin <eigenein@gmail.com>
2022-06-12 20:54:44 +0200

    FIX: proper importing of backends

--- tests/caches/test_async.py
+++ tests/caches/test_async.py
@@ -1,6 +1,7 @@
 from pytest import mark
 
-from cachetory import backends, serializers
+from cachetory import serializers
+from cachetory.backends import async_ as async_backends
 from cachetory.caches.async_ import Cache
 from tests.support import if_redis_enabled
 
@@ -11,7 +12,7 @@ _test_redis = mark.skipif("not config.getoption('test_redis')")
 async def test_get_set_in_memory():
     cache = Cache[int](
         serializer=serializers.from_url("pickle+zstd://"),
-        backend=(await backends.async_.from_url("memory://")),
+        backend=(await async_backends.from_url("memory://")),
     )
     await cache.set("foo", 42)
     assert await cache.get("foo") == 42
@@ -22,7 +23,7 @@ async def test_get_set_in_memory():
 async def test_get_set_in_redis():
     cache = Cache[int](
         serializer=serializers.from_url("pickle+zstd://pickle-protocol=5&compression-level=3"),
-        backend=(await backends.async_.from_url("redis://localhost:6379")),
+        backend=(await async_backends.from_url("redis://localhost:6379")),
     )
     async with cache:
         await cache.set("foo", 42)
--- tests/caches/test_sync.py
+++ tests/caches/test_sync.py
@@ -1,6 +1,7 @@
 from pytest import mark
 
-from cachetory import backends, serializers
+from cachetory import serializers
+from cachetory.backends import sync as sync_backends
 from cachetory.caches.sync import Cache
 from tests.support import if_redis_enabled
 
@@ -10,7 +11,7 @@ _test_redis = mark.skipif("not config.getoption('test_redis')")
 def test_get_set_in_memory():
     cache = Cache[int](
         serializer=serializers.from_url("pickle+zstd://pickle-protocol=5&compression-level=3"),
-        backend=backends.sync.from_url("memory://"),
+        backend=sync_backends.from_url("memory://"),
     )
     cache.set("foo", 42)
     assert cache.get("foo") == 42
@@ -20,7 +21,7 @@ def test_get_set_in_memory():
 def test_get_set_in_redis():
     cache = Cache[int](
         serializer=serializers.from_url("pickle+zstd://pickle-protocol=5&compression-level=3"),
-        backend=backends.sync.from_url("redis://localhost:6379"),
+        backend=sync_backends.from_url("redis://localhost:6379"),
     )
     with cache:
         cache.set("foo", 42)
