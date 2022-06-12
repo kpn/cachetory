972e84d3d32b6427e5ffb1a70eddbbb8b91d529c
Pavel Perestoronin <eigenein@gmail.com>
2022-06-12 20:51:04 +0200

    NEW: add `serialize_executor` to `async_.Cache`

--- cachetory/caches/async_.py
+++ cachetory/caches/async_.py
@@ -1,3 +1,5 @@
+from asyncio import get_running_loop
+from concurrent.futures import Executor
 from contextlib import AbstractAsyncContextManager
 from datetime import timedelta
 from typing import Dict, Generic, Iterable, Mapping, Optional, Tuple, Union
@@ -6,14 +8,24 @@ from cachetory.caches.private import DefaultT
 from cachetory.interfaces.backends.async_ import AsyncBackend
 from cachetory.interfaces.backends.private import WireT
 from cachetory.interfaces.serializers import Serializer, ValueT
+from cachetory.private.typing import NotSet
+
+_NOT_SET = NotSet()
 
 
 class Cache(AbstractAsyncContextManager, Generic[ValueT]):
-    __slots__ = ("_serializer", "_backend")
+    __slots__ = ("_serializer", "_backend", "_serialize_executor")
 
-    def __init__(self, *, serializer: Serializer[ValueT, WireT], backend: AsyncBackend[WireT]):
+    def __init__(
+        self,
+        *,
+        serializer: Serializer[ValueT, WireT],
+        backend: AsyncBackend[WireT],
+        serialize_executor: Union[None, Executor, NotSet] = _NOT_SET,
+    ):
         self._serializer = serializer
         self._backend = backend
+        self._serialize_executor = serialize_executor
 
     async def get(self, key: str, default: DefaultT = None) -> Union[ValueT, DefaultT]:  # type: ignore
         try:
@@ -21,10 +33,10 @@ class Cache(AbstractAsyncContextManager, Generic[ValueT]):
         except KeyError:
             return default
         else:
-            return self._serializer.deserialize(data)
+            return await self._deserialize(data)
 
     async def get_many(self, *keys: str) -> Dict[str, ValueT]:
-        return {key: self._serializer.deserialize(data) async for key, data in self._backend.get_many(*keys)}
+        return {key: await self._deserialize(data) async for key, data in self._backend.get_many(*keys)}
 
     async def expire_in(self, key: str, time_to_live: Optional[timedelta] = None) -> None:
         return await self._backend.expire_in(key, time_to_live)
@@ -38,7 +50,7 @@ class Cache(AbstractAsyncContextManager, Generic[ValueT]):
     ) -> None:
         await self._backend.set(
             key,
-            self._serializer.serialize(value),
+            await self._serialize(value),
             time_to_live=time_to_live,
             if_not_exists=if_not_exists,
         )
@@ -46,10 +58,24 @@ class Cache(AbstractAsyncContextManager, Generic[ValueT]):
     async def set_many(self, items: Union[Iterable[Tuple[str, ValueT]], Mapping[str, ValueT]]) -> None:
         if isinstance(items, Mapping):
             items = items.items()
-        await self._backend.set_many((key, self._serializer.serialize(value)) for key, value in items)
+        await self._backend.set_many((key, await self._serialize(value)) for key, value in items)
 
     async def delete(self, key: str) -> bool:
         return await self._backend.delete(key)
 
     async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
         return await self._backend.__aexit__(exc_type, exc_val, exc_tb)
+
+    async def _serialize(self, value: ValueT) -> WireT:
+        if self._serialize_executor is _NOT_SET:
+            return self._serializer.serialize(value)
+        else:
+            return await get_running_loop().run_in_executor(self._serialize_executor, self._serializer.serialize, value)
+
+    async def _deserialize(self, data: WireT) -> ValueT:
+        if self._serialize_executor is _NOT_SET:
+            return self._serializer.deserialize(data)
+        else:
+            return await get_running_loop().run_in_executor(
+                self._serialize_executor, self._serializer.deserialize, data
+            )
---
+++ cachetory/private/typing.py
@@ -0,0 +1,5 @@
+class NotSet:
+    """
+    Used to check for unset parameters where `None` is an acceptable value.
+    This also allows type-hinting of sentinel parameters.
+    """
