85d9c85a5706bf75f658767367a78c78be6d4636
Sergio Castillo <sergio.lara@elementsinteractive.es>
2023-06-14 16:03:26 +0200

    NEW: Allow sync or async callables for async `cached` decorator
    Both `cache` and `time_to_live` can now be either sync or async callables
    in the decorator.
    
    closes #16

--- cachetory/decorators/async_.py
+++ cachetory/decorators/async_.py
@@ -16,10 +16,14 @@ Original wrapped function parameter specification.
 
 
 def cached(
-    cache: Union[Cache[ValueT, WireT], Callable[..., Awaitable[Cache[ValueT, WireT]]]],  # no way to use `P` here
+    cache: Union[
+        Cache[ValueT, WireT],
+        Callable[..., Cache[ValueT, WireT]],
+        Callable[..., Awaitable[Cache[ValueT, WireT]]],
+    ],  # no way to use `P` here
     *,
     make_key: Callable[..., str] = shared.make_default_key,  # no way to use `P` here
-    time_to_live: Optional[Union[timedelta, Callable[..., Awaitable[timedelta]]]] = None,
+    time_to_live: Optional[Union[timedelta, Callable[..., timedelta], Callable[..., Awaitable[timedelta]]]] = None,
     if_not_exists: bool = False,
 ) -> Callable[[Callable[P, Awaitable[ValueT]]], Callable[P, Awaitable[ValueT]]]:
     """
@@ -27,13 +31,13 @@ def cached(
 
     Args:
         cache:
-            `Cache` instance or an async callable tha returns a `Cache` instance for each function call.
+            `Cache` instance or an callable (sync or async) that returns a `Cache` instance for each function call.
             In the latter case the specific callable gets called with a wrapped function as the first argument,
             and the rest of the arguments next to it.
         make_key: callable to generate a custom cache key per each call.
         if_not_exists: controls concurrent sets: if `True` – avoids overwriting a cached value.
         time_to_live:
-            cached value expiration time or async callable that returns the expiration time.
+            cached value expiration time or a callable (sync or async) that returns the expiration time.
             The callable needs to accept keyword arguments, and it is given the cache key to
             compute the expiration time.
     """
@@ -41,9 +45,15 @@ def cached(
     def wrap(callable_: Callable[P, Awaitable[ValueT]]) -> Callable[P, Awaitable[ValueT]]:
         @wraps(callable_)
         async def cached_callable(*args: P.args, **kwargs: P.kwargs) -> ValueT:
-            cache_ = await cache(callable_, *args, **kwargs) if callable(cache) else cache
+            cache_ = (
+                cache if not callable(cache) else await shared.call_sync_or_async(cache, callable_, *args, **kwargs)
+            )
             key_ = make_key(callable_, *args, **kwargs)
-            time_to_live_ = await time_to_live(key=key_) if callable(time_to_live) else time_to_live
+
+            time_to_live_ = (
+                time_to_live if not callable(time_to_live) else await shared.call_sync_or_async(time_to_live, key=key_)
+            )
+
             value = await cache_.get(key_)
             if value is None:
                 value = await callable_(*args, **kwargs)
