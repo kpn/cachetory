a1c4208d32e91a2ab6615ae8a034bbe426b032d1
Pavel Perestoronin <eigenein@gmail.com>
2023-07-12 14:38:50 +0200

    NEW: support cache skipping in `@cached` ✨

--- cachetory/decorators/sync.py
+++ cachetory/decorators/sync.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from contextlib import suppress
 from datetime import timedelta
 from functools import wraps
 from typing import Callable
@@ -17,10 +18,10 @@ P = ParamSpec("P")
 
 
 def cached(
-    cache: Cache[ValueT, WireT] | Callable[..., Cache[ValueT, WireT]],  # no way to use `P` here
+    cache: Cache[ValueT, WireT] | Callable[..., Cache[ValueT, WireT] | None] | None,  # no way to use `P` here
     *,
     make_key: Callable[..., str] = shared.make_default_key,  # no way to use `P` here
-    time_to_live: timedelta | Callable[..., timedelta] | None = None,
+    time_to_live: timedelta | Callable[..., timedelta | None] | None = None,
     if_not_exists: bool = False,
     exclude: Callable[[str, ValueT], bool] | None = None,
 ) -> Callable[[Callable[P, ValueT]], _CachedCallable[P, ValueT]]:
@@ -32,6 +33,7 @@ def cached(
             `Cache` instance or a callable tha returns a `Cache` instance for each function call.
             In the latter case the specified callable gets called with a wrapped function as the first argument,
             and the rest of the arguments next to it.
+            If the callable returns `None`, the cache is skipped.
         make_key: callable to generate a custom cache key per each call.
         time_to_live:
             cached value expiration time or callable that returns the expiration time.
@@ -49,19 +51,24 @@ def cached(
         def cached_callable(*args: P.args, **kwargs: P.kwargs) -> ValueT:
             cache_ = get_cache(callable_, *args, **kwargs)
             key_ = make_key(callable_, *args, **kwargs)
-            time_to_live_ = get_time_to_live(key=key_)
 
-            try:
-                value = cache_[key_]
-            except KeyError:
-                value = callable_(*args, **kwargs)
-                if exclude is None or not exclude(key_, value):
-                    cache_.set(key_, value, time_to_live=time_to_live_, if_not_exists=if_not_exists)
+            if cache_ is not None:
+                with suppress(KeyError):
+                    # `KeyError` normally means the value is «non-cached».
+                    return cache_[key_]
+
+            value = callable_(*args, **kwargs)
+            if cache_ is not None and (exclude is None or not exclude(key_, value)):
+                time_to_live_ = get_time_to_live(key=key_)
+                cache_.set(key_, value, time_to_live=time_to_live_, if_not_exists=if_not_exists)
             return value
 
         def purge(*args: P.args, **kwargs: P.kwargs) -> bool:
-            key = make_key(callable_, *args, **kwargs)
-            return get_cache(callable_, *args, **kwargs).delete(key)
+            if (cache := get_cache(callable_, *args, **kwargs)) is not None:
+                key = make_key(callable_, *args, **kwargs)
+                return cache.delete(key)
+            else:
+                return False
 
         cached_callable.purge = purge  # type: ignore[attr-defined]
         return cached_callable  # type: ignore[return-value]
